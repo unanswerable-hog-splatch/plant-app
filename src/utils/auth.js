1219fb5dcd1055f6615935ef8ab1d73c3b696148
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-13 15:04:26 -0700

    added authService

---
+++ src/utils/auth.js
@@ -0,0 +1,42 @@
+// Use this to decode a token and get hte user's information out of it
+import decode from 'jwt-decode';
+
+// Create a new class to instantiate for a user
+class AuthService {
+  // get user data
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  // Check if user's logged in
+  isLoggedIn() {
+    // Checks if there is a saved token and whether or not it's still valid
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
+
+  isTokenExpired(token) {
+    const decoded = decode(token);
+    if(decoded.exp < Date.now() / 1000) {
+      localStorage.removeItem('id_token');
+      return true;
+    }
+    return false;
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStoage.removeItem('id_token');
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
