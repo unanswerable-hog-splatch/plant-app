a4a2c223621c145d1531630832d560a474232246
Sam Page <sampage6@gmail.com>
2022-03-16 01:31:14 -0500

    signin function works

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,22 +7,15 @@ import Calendar from '../components/calendar/Calendar';
 export default function Home() {
   return (
     <>
-    <Calendar />
+    {/* <Calendar /> */}
       {/* If user is logged in, the calendar is displayed. If not, Login and Sign Up buttons are displayed */}
       {Auth.isLoggedIn() ? <Calendar />
         : (
           <>
-            {/* <Link to='/login'>
-              <button className=''>
-                Login
-              </button>
-            </Link>
-            <Link to='/signup'>
-              <button className=''>
-                Signup
-              </button>
-            </Link> */}
-          </>)}
+              HELLOOOOOOO
+          </>
+          )
+          }
     </>
   );
 }
