7963cb28eb3cea0875f2860f367d34632ddf1241
Sam Page <sampage6@gmail.com>
2022-03-16 19:49:13 -0500

    added logout functionality

--- src/components/nav/LoginForm.js
+++ src/components/nav/LoginForm.js
@@ -1,114 +1,114 @@
-// import { useMutation } from '@apollo/client';
-// import React, { useState } from 'react';
-// import { Form, Input, Button, Checkbox } from 'antd';
-// import Auth from '../../utils/auth';
-// import { LOGIN_GARDENER } from '../../utils/mutations';
-// // export default function SignupForm() {
+import { useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import Auth from '../../utils/auth';
+import { LOGIN_GARDENER } from '../../utils/mutations';
+// export default function SignupForm() {
 
-// const LoginForm = () => {
+const LoginForm = () => {
 
-//     const onFinishFailed = (errorInfo) => {
-//         console.log('Failed:', errorInfo);
-//     };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
 
-//     const [gardenerFormData, setGardenerFormData] = useState({ email: '', password: '' });
+    const [gardenerFormData, setGardenerFormData] = useState({ email: '', password: '' });
 
 
-//     const handleInputChange = (event) => {
-//         const { name, value } = event.target;
-//         setGardenerFormData({ ...gardenerFormData, [name]: value });
-//     };
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setGardenerFormData({ ...gardenerFormData, [name]: value });
+    };
 
-//     const [login] = useMutation(LOGIN_GARDENER )
+    const [login] = useMutation(LOGIN_GARDENER )
 
-//     const onFinish = async (values) => {
-//         // event.preventDefault();
-//         console.log(values)
-//         // gardenerFormData = event
-//         // console.log(gardenerFormData)
+    const onFinish = async (values) => {
+        // event.preventDefault();
+        console.log(values)
+        // gardenerFormData = event
+        // console.log(gardenerFormData)
 
-//         try {
-//             const { data } = await login({
-//                 variables: { ...values }
-//             })
-//             console.log(data)
-//             Auth.login(data.addGardener.token)
-//         } catch (err) {
-//             console.error(err);
-//         }
+        try {
+            const { data } = await login({
+                variables: { ...values }
+            })
+            console.log(data)
+            Auth.login(data.addGardener.token)
+        } catch (err) {
+            console.error(err);
+        }
 
-//         setGardenerFormData({
-//             email: '',
-//             password: '',
-//         });
-//     };
+        setGardenerFormData({
+            email: '',
+            password: '',
+        });
+    };
 
-//     return (
-//         <Form
-//             name="basic"
-//             labelCol={{
-//                 span: 8,
-//             }}
-//             wrapperCol={{
-//                 span: 16,
-//             }}
-//             initialValues={{
-//                 remember: true,
-//             }}
-//             // onSubmit={handleFormSubmit}
-//             onFinish={onFinish}
-//             onFinishFailed={onFinishFailed}
-//             autoComplete="off"
-//         >
+    return (
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 16,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            // onSubmit={handleFormSubmit}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
 
-//             <Form.Item
-//                 label="Email"
-//                 name="email"
-//                 onChange={handleInputChange}
-//                 value={gardenerFormData.email}
-//                 rules={[
-//                     {
-//                         required: true,
-//                         message: 'Please input your email!',
-//                     },
-//                 ]}
-//             >
-//                 <Input />
-//             </Form.Item>
+            <Form.Item
+                label="Email"
+                name="email"
+                onChange={handleInputChange}
+                value={gardenerFormData.email}
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your email!',
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
 
-//             <Form.Item
-//                 label="Password"
-//                 name="password"
-//                 onChange={handleInputChange}
-//                 value={gardenerFormData.password}
-//                 rules={[
-//                     {
-//                         required: true,
-//                         message: 'Please input your password!',
-//                     },
-//                 ]}
-//             >
-//                 <Input.Password />
-//             </Form.Item>
+            <Form.Item
+                label="Password"
+                name="password"
+                onChange={handleInputChange}
+                value={gardenerFormData.password}
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your password!',
+                    },
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
 
-//             <Form.Item
-//                 wrapperCol={{
-//                     offset: 8,
-//                     span: 16,
-//                 }}
-//             >
-//                 <Button
-//                     // disabled={!(gardenerFormData.name && gardenerFormData.email && gardenerFormData.password)}
-//                     type="primary"
-//                     htmlType="submit">
-//                     Submit
-//                 </Button>
-//             </Form.Item>
-//         </Form>
-//     );
-// };
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                }}
+            >
+                <Button
+                    // disabled={!(gardenerFormData.name && gardenerFormData.email && gardenerFormData.password)}
+                    type="primary"
+                    htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
 
-// export default LoginForm;
+export default LoginForm;
 
 
 
--- src/components/nav/SignupForm.js
+++ src/components/nav/SignupForm.js
@@ -1,11 +1,14 @@
 import { useMutation } from '@apollo/client';
 import React, { useState } from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Modal, Checkbox } from 'antd';
 import Auth from '../../utils/auth';
 import { ADD_GARDENER } from '../../utils/mutations';
+import LoginForm from './LoginForm';
+
 // export default function SignupForm() {
 
 const SignupForm = () => {
+    const [visible, setVisible] = useState(false);
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
@@ -45,81 +48,96 @@ const SignupForm = () => {
     };
 
     return (
-        <Form
-            name="basic"
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 16,
-            }}
-            initialValues={{
-                remember: true,
-            }}
-            // onSubmit={handleFormSubmit}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="Name"
-                name="name"
-                onChange={handleInputChange}
-                value={gardenerFormData.name}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your name!',
-                    },
-                ]}
+        <>
+            <Button type="primary" onClick={() => setVisible(true)}>
+                Open Modal of 1000px width
+            </Button>
+            <Modal
+                title="Modal 1000px width"
+                centered
+                visible={visible}
+                onOk={() => setVisible(false)}
+                onCancel={() => setVisible(false)}
+                width={1000}
             >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Email"
-                name="email"
-                onChange={handleInputChange}
-                value={gardenerFormData.email}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your email!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Password"
-                name="password"
-                onChange={handleInputChange}
-                value={gardenerFormData.password}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item
+                < LoginForm />
+            </Modal>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
                 wrapperCol={{
-                    offset: 8,
                     span: 16,
                 }}
+                initialValues={{
+                    remember: true,
+                }}
+                // onSubmit={handleFormSubmit}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
-                <Button
-                    // disabled={!(gardenerFormData.name && gardenerFormData.email && gardenerFormData.password)}
-                    type="primary"
-                    htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    onChange={handleInputChange}
+                    value={gardenerFormData.name}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your name!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    onChange={handleInputChange}
+                    value={gardenerFormData.email}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    onChange={handleInputChange}
+                    value={gardenerFormData.password}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button
+                        // disabled={!(gardenerFormData.name && gardenerFormData.email && gardenerFormData.password)}
+                        type="primary"
+                        htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
     );
 };
 
