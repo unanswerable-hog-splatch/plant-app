ba1840664c7e0c5f20e2f9909eb4199cbba9ade6
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-19 20:50:43 -0700

    moved today, oneDay to useHook

--- src/components/addPlant/AddPlantForm.js
+++ src/components/addPlant/AddPlantForm.js
@@ -20,43 +20,20 @@ import { useMutation } from '@apollo/client';
 import Auth from '../../utils/auth';
 import { ADOPT_PLANT } from '../../utils/mutations';
 
+import useHook from '../../hooks/useHook';
+
 export default function AddPlantForm({ addPlantVisible, setAddPlantVisible }) {
-  const today = (new Date().setHours(0, 0, 0, 0)) / 1000;
-  const oneDay = 60 * 60 * 24;
   const [adoptPlant] = useMutation(ADOPT_PLANT);
   const [fertilizerVisible, setFertilizerVisible] = useState(false);
   const [plantIcon, setPlantIcon] = useState('cactus');
+
+  const { convertFrequency, camelCase, today, oneDay } = useHook();
 
   const categories = ['Cactus', 'Shrubbery', 'Herb', 'Succulent', 'Tree', 'Medicinal', 'Flower', 'Foliage', 'Fern', 'Hanging', 'Fake', 'Christmas'];
   const plantIcons = ['Cactus', 'Snake Plant', 'Aloe Vera', 'Bonsai Tree', 'Basil'];
   const frequencyUnits = ['day', 'week', 'month', 'year'];
 
-  // Takes in a number and unit of time and returns an equivelant number of days
-  const convertFrequency = (amount, unit) => {
-    let multiplier;
-    switch (unit) {
-      default: multiplier = 1;
-        break;
-      case ('week'): multiplier = 7;
-        break;
-      case ('month'): multiplier = 30;
-        break;
-      case ('year'): multiplier = 365;
-        break;
-    }
-    return multiplier * amount;
-  }
 
-  // Converts a string to camel case for select option values
-  const camelCase = (str) => {
-    const words = str.toLowerCase().split(' ');
-    let result = words[0];
-    for (let i = 1; i < words.length; i++) {
-      result += words[i].charAt(0).toUpperCase();
-      result += words[i].slice(1);
-    }
-    return result;
-  }
 
   // This is what we do instead of onSubmit I guess
   const onFinish = async (values) => {
--- src/hooks/useHook.js
+++ src/hooks/useHook.js
@@ -1,34 +1,39 @@
 export default function useHook() {
 
-    // Takes in a number and unit of time and returns an equivelant number of days
-    const convertFrequency = (amount, unit) => {
-      let multiplier;
-      switch (unit) {
-        default: multiplier = 1;
-          break;
-        case ('week'): multiplier = 7;
-          break;
-        case ('month'): multiplier = 30;
-          break;
-        case ('year'): multiplier = 365;
-          break;
-      }
-      return multiplier * amount;
-    }
-  
-    // Converts a string to camel case for select option values
-    const camelCase = (str) => {
-      const words = str.toLowerCase().split(' ');
-      let result = words[0];
-      for (let i = 1; i < words.length; i++) {
-        result += words[i].charAt(0).toUpperCase();
-        result += words[i].slice(1);
-      }
-      return result;
-    }
+  const today = (new Date().setHours(0, 0, 0, 0)) / 1000;
+  const oneDay = 60 * 60 * 24;
 
-    return {
-      convertFrequency,
-      camelCase,
+  // Takes in a number and unit of time and returns an equivelant number of days
+  const convertFrequency = (amount, unit) => {
+    let multiplier;
+    switch (unit) {
+      default: multiplier = 1;
+        break;
+      case ('week'): multiplier = 7;
+        break;
+      case ('month'): multiplier = 30;
+        break;
+      case ('year'): multiplier = 365;
+        break;
     }
+    return multiplier * amount;
+  }
+
+  // Converts a string to camel case for select option values
+  const camelCase = (str) => {
+    const words = str.toLowerCase().split(' ');
+    let result = words[0];
+    for (let i = 1; i < words.length; i++) {
+      result += words[i].charAt(0).toUpperCase();
+      result += words[i].slice(1);
+    }
+    return result;
+  }
+
+  return {
+    convertFrequency,
+    camelCase,
+    oneDay,
+    today
+  }
 }
