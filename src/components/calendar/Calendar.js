ed0b11798a81ab9913811b553fde8d42030af44c
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-20 19:19:11 -0500

    CalendarDay and Calendar pseudocode

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -12,7 +12,7 @@ export default function Calendar({ loading, gardenerData }) {
   const firstDayOfWeek = new Date(firstDay).getDay();
   const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate()
 
-  // //List of month names to populate in front of number
+  // List of month names to populate in front of number
   // const monthList = ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
   let currentWeek = 0;
@@ -53,11 +53,18 @@ export default function Calendar({ loading, gardenerData }) {
 /*------------------------------- CALENDAR JSX -------------------------------*/
   return (
     // Calendar class is a flex column
-    /* Create the header with each individual week day name - should be inside the calendar div but no styling the same of the day */
+
+    /* 
+    -Create a div with the current month about the days of the week OR have each individual day have the calendar month before the number.
+      --if do in the return, need to add <> </> around because it would not be a flex column 
+    - Create the header with each individual week day name - should be inside the calendar div but no styling the same of the day
+    
+    */
     <div className="calendar">
       {/* Add in the days of the week here in its own div so that the flex column align with the daily ones */}
       {monthArray.map(week => {
         // Week class is a flex row
+        /* Would it make since to have the day be living ere and then within the created boxes have the plants and date stuff??? */ 
         return (<div className="week">
           {week}
         </div>
