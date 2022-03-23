07d99db66ed9a6d962385e289ea615b504e482a3
John Hur <hur.john628@gmail.com>
2022-03-23 00:31:01 -0500

    fixed proxy stuff for dev

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ import Footer from './components/footer/Footer';
 
 // COMMENT BACK IN WHEN USING HEROKU
 
-const uri = process.env.NODE_ENV === 'development'
-  ?'/graphql' : 'https://shelf-care-backend.herokuapp.com/graphql';
+// const uri = process.env.NODE_ENV === 'development'
+//   ?'/graphql' : 'https://shelf-care-backend.herokuapp.com/graphql';
 
 //COMMENT OUT WHEN USING HEROKU
 
 const httpLink = createHttpLink({
-  uri: uri
+  uri: '/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
